e7ea5d5a9285aae311da697f178eabd2c4f894b7
zayooko <zayookoAR@gmail.com>
2021-10-30 19:41:39 +0200

    CoinDestroy.cs void update

--- Assets/Scripts/Coin/CoinDestroy.cs
+++ Assets/Scripts/Coin/CoinDestroy.cs
@@ -2,14 +2,51 @@
 //using System.Collections.Generic;
 using UnityEngine;
 
+//using System.Threading.Tasks;
+
 public class CoinDestroy : MonoBehaviour
+
 {
+    // public GameObject coinPrefab1;
+    // public GameObject coinPrefab2;
+    // // public GameObject coin;
+    // // public GameObject coinRed;
+    // // public GameObject CoinContaniner;
+    // // public GameObject CoinRedContainer;
+    
+    // void Start() 
+    // {
+    //     // coinPrefab1 = GameObject.Find("coin");
+    //     // coinPrefab2 = GameObject.Find("coinRed");    
+    // }
+
+   
+    
+    //private async void Update()
+
     void Update()
     {
-        if(gameObject.transform.position.z >= 4)
+        //if(GameObject.Find("coin").transform.position.z >= 4)
+        if(gameObject.transform.position.z >= 4 && gameObject != null)
         {
-            //Debug.Log("Ok");
-            Destroy(transform.parent.gameObject); 
+            Debug.Log("Ok1");
+
+            
+             //await Task.Delay(10000);
+
+            // //Destroy(GameObject.Find("coin")); 
+            Destroy(transform.parent.gameObject);
+            
+            //gameObject.SetActive(false);
+        }
+        else
+        {
+            DontDestroyOnLoad(transform.parent.gameObject);
         }    
+    //     // if(GameObject.Find("coinRed").transform.position.z >= 4)
+    //     // {
+    //     //     Debug.Log("Ok2");
+    //     //     Destroy(GameObject.Find("coinRed"));     
+    //     // }    
     }
 }
