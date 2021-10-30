cdcb04ddcdaf9abc72e60444aed1cabd54f96545
zayooko <zayookoAR@gmail.com>
2021-10-30 19:42:55 +0200

    CoinSpawner.cs void Update

--- Assets/Scripts/Coin/CoinSpawner.cs
+++ Assets/Scripts/Coin/CoinSpawner.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class CoinSpawner : MonoBehaviour
 {
 
-    float timer;
-    public GameObject coinPrefab;
+    float timer1;
+    float timer2;
+
+    public GameObject coinPrefab1;
+    public GameObject coinPrefab2;
+
+
+    // void Start() 
+    // {
+    //     coinPrefab1 = GameObject.Find("CoinContainer");
+    //     coinPrefab2 = GameObject.Find("CoinRedContainer");    
+    // }
+
 
     void Update()
     {
-        timer += Time.deltaTime;
+        
+ 
+        
+        
+        timer1 += Time.deltaTime;
 
-        if(timer >= 2f)
+        if(timer1 >= 2f)
         {
-            timer = 0;
+            timer1 = 0;
             float x = Random.Range(-10f, 10f);
             Vector3 position = new Vector3(x, 0, 0);
             Quaternion rotation = new Quaternion();
-            Instantiate(coinPrefab, position, rotation);
+            Instantiate(coinPrefab1, position, rotation);
+            //coinPrefab1 = GameObject.Find("CoinContainer(Cloner)");
+  
+
+        }
+
+
+        timer2 += Time.deltaTime;
+
+        if(timer2 >= 3f)
+        {
+            timer2 = 0;
+            float x = Random.Range(-10f, 10f);
+            Vector3 position = new Vector3(x, 0, 0);
+            Quaternion rotation = new Quaternion();
+            Instantiate(coinPrefab2, position, rotation);
+            //coinPrefab2 = GameObject.Find("CoinRedContainer(Cloner)");
         }  
+
+
+
+
     }
 }
